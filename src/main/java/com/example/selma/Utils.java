3bc9bff8324b41c5992d3a9664f5aa9062f76208
Sylvia-friendly <landheernoam@gmail.com>
2025-07-31 01:25:30 +0200

    Create Utils.java

---
+++ src/main/java/com/example/selma/Utils.java
@@ -0,0 +1,9 @@
+package com.example.selma;
+
+import net.minecraft.util.Identifier;
+
+public class Utils {
+    public static Identifier id(String path) {
+        return new Identifier("selmablockmod", path);
+    }
+}
